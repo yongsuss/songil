0ddfbd5adb2b7514c0f97526b7c3685921846506
starfish22 <88958856+kgh5865@users.noreply.github.com>
2024-05-29 01:36:48 +0900

    v1.0.6
    azure storage에 프로필 이미지 업로드

--- screens/Profile.js
+++ screens/Profile.js
@@ -186,6 +186,7 @@ import axios from 'axios';
 import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, ScrollView } from 'react-native';
 import { AppContext } from '../AppContext';
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 const ProfileScreen = ({ navigation, setIsLoggedIn }) => {
   const { apiUrl, id } = useContext(AppContext); // AppContext에서 userId를 가져옴
@@ -245,19 +246,46 @@ const ProfileScreen = ({ navigation, setIsLoggedIn }) => {
   }, [apiUrl, id]);
 
   const handleChoosePhoto = async () => {
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();//파일 접근 권한
     if (permissionResult.granted === false) {
       alert("We need permission to access your photos!");
       return;
     }
 
-    const result = await ImagePicker.launchImageLibraryAsync({
+    const result = await ImagePicker.launchImageLibraryAsync({//이미지 가져오기
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
     });
 
-    if (!result.cancelled) {
-      setProfileImage(result.assets[0].uri);
+    if (!result.cancelled) {//이미지를 선택했을 때
+      const fileUri = result.assets[0].uri;
+      const fileName = fileUri.split('/').pop();
+      console.log(fileName);
+      setProfileImage(fileUri);
+
+      // SAS 토큰 및 Azure Storage URL 설정 (여기서는 예시로 작성함)
+  const sasToken = "sp=racwdl&st=2024-05-28T15:49:49Z&se=2024-06-30T23:49:49Z&sv=2022-11-02&sr=c&sig=q%2BwvZ6cJc6m7HnmAA%2FgTXn7lBsD1vdVpgZiqLSx4OuE%3D";
+  const azureUrl = `https://songilstorage.blob.core.windows.net/songil/${fileName}?${sasToken}`;
+  
+
+  // 파일을 ArrayBuffer로 변환
+  const file = await FileSystem.readAsStringAsync(fileUri, { encoding: FileSystem.EncodingType.Base64 });
+  const fileArrayBuffer = Uint8Array.from(atob(file), c => c.charCodeAt(0));
+
+  // Axios를 사용해 Azure Storage에 업로드
+  try {
+    await axios.put(azureUrl, fileArrayBuffer, {
+      headers: {
+        'x-ms-blob-type': 'BlockBlob',
+        'Content-Type': 'image/jpeg' // 파일 타입에 따라 변경 가능
+      }
+    });
+    console.log('Upload successful');
+    //업로드한 사진의 url 형식
+    //https://songilstorage.blob.core.windows.net/songil/<file-name>
+  } catch (error) {
+    console.error('Error uploading file:', error);
+  }
     }
   };
 
